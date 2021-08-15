746b248f9d42b70b43ab4a49e5258cb16b7896f0
qti <84819214+qu3tiiii@users.noreply.github.com>
2021-08-15 13:48:05 -0400

    Create DS-BOT.py

---
+++ DS-BOT.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+import datetime
+#Bot de discord simple en 12 lineas<3
+from urllib import parse, request
+import re
+                               #Prefijo del bot
+bot = commands.Bot(command_prefix='!', description="Tutorial Bot/Undead")
+#1
+@bot.command()
+         #Mensaje a responder
+async def Hola(ctx):
+                    #Respuesta
+    await ctx.send('Hola undead, siganlo en tiktok')
+#2
+
+#Desde 1 hasta 2 pueden copiar y pegar cambiando los mensajes de respuesta etc
+#Actividad del bot
+@bot.event
+async def on_ready():
+   print('Estoy listo!:)')
+           #Token del bot
+bot.run('Token aca')
