b56dfc76d7b3eea1c861930d3df76a946d988b62
qti <84819214+qu3tiiii@users.noreply.github.com>
2021-10-12 12:38:37 -0300

    Update DS-BOT.py

--- DS-BOT.py
+++ DS-BOT.py
@@ -11,7 +11,7 @@ bot = commands.Bot(command_prefix='!', description="Tutorial Bot/Undead")
          #Mensaje a responder
 async def Hola(ctx):
                     #Respuesta
-    await ctx.send('Hola undead, siganlo en tiktok')
+    await ctx.send('Hola ')
 #2
 
 #Desde 1 hasta 2 pueden copiar y pegar cambiando los mensajes de respuesta etc
